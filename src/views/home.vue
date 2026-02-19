66f8863733e672f352b725f9dfbb17f913b9da85
radoslaw.kumorek <radoslaw.kumorek@kaseya.com>
2026-02-19 15:27:52 +0100

    'i' in the center

--- src/views/home.vue
+++ src/views/home.vue
@@ -13,7 +13,7 @@
       </div>
       <div class="hero-content">
         <div class="hero-names">
-          <h1 class="hero-title home-hero-title">Klaudia i Radosław</h1>
+          <h1 class="hero-title home-hero-title"><span>Klaudia</span> <span>i</span> <span>Radosław</span></h1>
           <div class="hero-heart">
               <path
                 d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676a.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"
@@ -109,6 +109,14 @@ export default {
 
 .hero-title {
   font-family: 'Qwitcher Grypen', cursive;
+  
+}
+
+@media (max-width: 768px) {
+  .hero-title span {
+    display: block;
+    line-height: 1;
+  }
 }
 
 .home-container1 {
