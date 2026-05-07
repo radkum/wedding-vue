c92cb4537fe48a1a5711bf0fc1c8c2fbd25a1e94
radkum <radoslaw.kumorekit@gmail.com>
2026-05-07 11:21:29 +0200

    Dodano kwiaty i wino

--- src/views/home.vue
+++ src/views/home.vue
@@ -27,6 +27,7 @@
         </div>
         <div class="hero-date">
           <span class="hero-date-text">30 Maj 2026</span>
+          <img src="/kwiaty_2.png" alt="Kwiaty" class="hero-flowers" />
         </div>
         <p class="hero-message section-content">
         </p>
